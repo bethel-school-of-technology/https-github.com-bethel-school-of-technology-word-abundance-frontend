506b7d9db8efeefb42a73192423966672d7ede8b
Benjamin Chacko <benjaminchacko@outlook.com>
2019-07-30 20:58:13 -0500

    Update SideNav.js

--- src/pages/Layout/SideNav.js
+++ src/pages/Layout/SideNav.js
@@ -1,41 +1,37 @@
-import React, { Component } from 'react';
-import { Button, Fade } from 'reactstrap';
-
-class SideNav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { fadeIn: false };
-        this.toggle = this.toggle.bind(this);
-    }
-    render() {
-        return (
-            <div>
-                <br/>
-                <Button color="primary" onClick={this.toggle}>Product Categories</Button>
-                <Fade in={this.state.fadeIn} tag="h5" className="mt-3">
-                    <h3>Product Categories</h3>
-                    <ul>
-                        <li><a href="/maintenance">Maintenance</a></li>
-                        <li><a href="/Tech">Tech</a></li>
-                        <li><a href="/VisualArts">Visual Arts</a></li>
-                        <li><a href="/Music">Music</a></li>
-                        <li><a href="/BusinessServices">Business</a></li>
-                        <li><a href="/Administration">Administrative</a></li>
-                    </ul>
-                </Fade>
-            </div>
-        );
-    }
-
-    toggle() {
-        this.setState({
-            fadeIn: !this.state.fadeIn
-        });
-    }
+import React from 'react';
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
+class SideNav extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+       <Navbar expand="false">
+       <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          <NavDropdown title="Product Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item><a href="/maintenance">Maintenance</a></NavDropdown.Item>
+              <NavDropdown.Item><a href="/Tech">Tech</a></NavDropdown.Item>
+              <NavDropdown.Item><a href="/VisualArts">Visual Arts</a></NavDropdown.Item>
+              <NavDropdown.Item><a href="/Tech">Tech</a></NavDropdown.Item>
+              <NavDropdown.Item><a href="/VisualArts">Visual Arts</a></NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Service Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/2.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/2.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    
+      </React.Fragment>
+    )
+  }
 };
-
-
-
-
 
 export default SideNav;
