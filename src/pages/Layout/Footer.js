eddf50c634b9245fe7bf9cd150715bb52a927551
Benjamin Chacko <benjaminchacko@outlook.com>
2019-07-30 21:11:28 -0500

    Create Footer.js

---
+++ src/pages/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+    <React.Fragment>
+    <footer>
+      Copyright &copy; {new Date().getFullYear()} Abundance App
+    </footer>
+    </React.Fragment>
+    )
+}
+
+export default Footer;
