1884f5d89eeb41c95f25e0f4023f440f48235b19
Benjamin Chacko <benjaminchacko@outlook.com>
2019-08-05 09:47:25 -0500

    added Footer.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './Navbar';
+import Footer from './pages/Layout/Footer';
+import './App.css'
 
 export default ({ children }) => {
   return (
-    <div>
+    <React.Fragment>
       <NavBar />
       {children}
-    </div>
+      <Footer />
+    </React.Fragment>
   );
 };
