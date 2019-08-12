e804f9e761e5dc07d67734b2aa5c9fb4320d4b9b
Olivia Wall <theoliviakatelyn@gmail.com>
2019-08-12 13:27:14 -0600

    got rid of unused imports

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import Rate from '../Card/Rate';
+// import Rate from './Rate'
 // import {Button} from '../Button';
 class Card extends Component {
     render() {
         return (
-
-       
-<div>
+    
+        <div>
             <div className='container-fluid'>
                 <div className='row'>
                     <div className='col-sm-12'>
@@ -14,7 +13,7 @@ class Card extends Component {
 
                             <div className="card">
                                 <div className="content">
-                                    <Rate/>
+                                    {/* <Rate/> */}
                                     <div className="header">Description Of Service</div>
                                     <div className="description">
                                         <p>By <a href='https://www.facebook.com/olivia.k.wall'>Olivia</a></p>
