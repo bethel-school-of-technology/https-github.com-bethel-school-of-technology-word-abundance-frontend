535cf1842fe6e997f16e2ac67459015aeb1feadd
Benjamin Chacko <benjaminchacko@outlook.com>
2019-08-20 14:31:48 -0500

    updated authToken.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import { AUTH_USER, AUTH_ERROR } from './types';
 export const signup = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post(
-      'http://localhost:3001/user/signup',
+      'http://localhost:3001/signup',
       formProps
     );
 
@@ -19,7 +19,7 @@ export const signup = (formProps, callback) => async dispatch => {
 export const signin = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post(
-      'http://localhost:3001/user/login',
+      'http://localhost:3001/login',
       formProps
     );
 
